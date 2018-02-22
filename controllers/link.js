a857681a3eb3da42104fe3edf1c7fe84759d02e2
Brandon Sears <brandonsears8@gmail.com>
2018-02-22 11:35:10 -0500

    added index and new, functional database

--- controllers/link.js
+++ controllers/link.js
@@ -5,9 +5,30 @@ const Link = require('../models/Link');
 
 router.get("/", (req, res) => {
     Link.find({})
-        .then(link => {
-            res.render("link/index", { link })
+        .then(links => {
+            res.render("links/index", { links })
         })
 });
+
+router.post('/', (req, res) => {
+    Link.create({
+        url: req.body.url,
+        title: req.body.title,
+        description: req.body.description,
+        upload_date: req.body.upload_date
+    }).then(place => {
+        res.redirect('/links')
+      })
+})
+router.get('/new', (req, res) => {
+    res.render('links/new')
+  })
+
+router.get('/:id', (req, res) => {
+    Link.findOne({ _id: req.params.id})
+      .then(link => {
+        res.render('links/show', link)
+      })
+  })
 
 module.exports = router;
