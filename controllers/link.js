7ed45fd92311d4d4fdd8fec45635b4ff277feccc
Brandon Sears <brandonsears8@gmail.com>
2018-02-22 11:49:00 -0500

    finished basics

--- controllers/link.js
+++ controllers/link.js
@@ -24,11 +24,28 @@ router.get('/new', (req, res) => {
     res.render('links/new')
   })
 
+router.get('/edit/:id', (req, res) => {
+    Link.findOne({_id: req.params.id}).then(link => {
+        res.render('links/edit', link)
+    })
+})
+router.put('/:id', (req, res) => {
+    Link.findOneAndUpdate({ _id: req.params.id}, req.body, {new :true})
+    .then(link => {
+        res.redirect("/links")
+    })
+})
+router.delete('/:id', (req, res) => {
+    Link.findOneAndRemove({ _id: req.params.id }).then(() => {
+        res.redirect("/links")
+    })
+})
+
 router.get('/:id', (req, res) => {
     Link.findOne({ _id: req.params.id})
       .then(link => {
         res.render('links/show', link)
-      })
-  })
+    })
+})
 
 module.exports = router;
